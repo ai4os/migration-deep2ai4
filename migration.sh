40ff35dffe83fa1813e13cd849572f7126bb10c5
valentin.kozlov <valentin.kozlov@kit.edu>
2024-03-03 00:43:54 +0100

    feat: Update Jenkins CI/CD, replace URLs in existing files

--- migration.sh
+++ migration.sh
@@ -56,11 +56,13 @@ function replace-old-urls()
    -e "s,${DEEP_DOCKERFILE_REPO_URL},${AI4_CODE_REPO_URL},gI" \
    -e "s,deephdc/${DEEP_DOCKERFILE_REPO},ai4oshub/${AI4_CODE_REPO},gI" \
    -e "s,${DEEP_CODE_REPO_URL},${AI4_CODE_REPO_URL},gI" \
-   -e "s,${DEEP_JENKINS_REPO_BADGE},${AI4_JENKINS_REPO_BADGE},gI" \
-   -e "s,${DEEP_JENKINS_REPO_URL},${AI4_JENKINS_REPO_URL},gI" $1
+   -e "s,${DEEP_DOCKERFILE_REPO_JENKINS_BADGE},${AI4_CODE_REPO_JENKINS_BADGE},gI" \
+   -e "s,${DEEP_DOCKERFILE_REPO_JENKINS_URL},${AI4_CODE_REPO_JENKINS_URL},gI" \
+   -e "s,${DEEP_CODE_REPO_JENKINS_BADGE},${AI4_CODE_REPO_JENKINS_BADGE},gI" \
+   -e "s,${DEEP_CODE_REPO_JENKINS_URL},${AI4_CODE_REPO_JENKINS_URL},gI" $1
 }
 
-# function to check any mention of nc.deep-hybrid-datacloud.eu
+# function to check for any mention of nc.deep-hybrid-datacloud.eu
 function check-old-nc()
 {
   local file=$1
@@ -82,7 +84,7 @@ function check-old-nc()
 
 # 1. Configure repo URLs, retrieve names of defaults branches
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "1.'Configure Repos': We now configure old (deephdc) and new (ai4os) repos"
+echo "1. Configure Repos: configure old (deephdc) and new (ai4os) repos URLs   "
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 read -p "Press enter to continue"
 
@@ -117,7 +119,7 @@ fi
 # Mirror old code repo to ai4os-hub
 echo ""
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "2.'Combined old deephdc code':"
+echo "2. Combine old deephdc code:"
 echo " * mirror ${DEEP_CODE_REPO_URL} to ${AI4_CODE_REPO_URL}"
 echo " * copy Dockerfile and metadata.json from ${DEEP_DOCKERFILE_REPO_URL}"
 echo " * rename branch, if requested"
@@ -158,21 +160,85 @@ cp ../${DEEP_DOCKERFILE_REPO}/metadata.json ./
 mkdir docker
 touch docker/.gitkeep
 git add Dockerfile metadata.json docker/*
-git commit -a -m "feat: migration-1, Add original Dockerfile, metadata.json"
 echo "[INFO] Added original Dockerfile, metadata.json, now pushing changes to ai4os-hub/"
+git commit -a -m "feat: migration-1, Add original Dockerfile, metadata.json"
 git push origin
 
-# 3. Delete/Re-add Jenkinsfile
+
+# 3. Automatically UPDATE EXISTING files to replace URLs of:
+# deepaas_repo, dockerfile_repo, docker_registry_repo, code, jenkins_badge, jenkins_url
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "3.'Re-create Jenkins CI/CD': "
-echo " * re-create Jenkinsfile"
-echo " * create .sqa/config.yml"
-echo " * create .sqa/docker-compose.yml"
+echo "3. Update existing files for AI4OS URL values of:"
+echo "   deepaas_repo, dockerfile_repo, docker_registry_repo, code, jenkins_badge, jenkins_url, in: "
+echo " * metadata.json"
+echo " * setup.cfg"
+echo " * README.md"
 echo " * commit changes to ${AI4_CODE_REPO_URL}"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 read -p "Press enter to continue"
+DEEP_DEEPAAS_REPO_URL="https://github.com/indigo-dc/DEEPaaS"
+DEEP_DOCKERFILE_REPO_JENKINS_BADGE="https://jenkins.indigo-datacloud.eu/buildStatus/icon?job=Pipeline-as-code/DEEP-OC-org/${DEEP_DOCKERFILE_REPO}/${DEEP_DOCKERFILE_REPO_BRANCH}"
+DEEP_DOCKERFILE_REPO_JENKINS_URL="https://jenkins.indigo-datacloud.eu/job/Pipeline-as-code/job/DEEP-OC-org/job/${DEEP_DOCKERFILE_REPO}/job/${DEEP_DOCKERFILE_REPO_BRANCH}"
+DEEP_CODE_REPO_JENKINS_BADGE="https://jenkins.indigo-datacloud.eu/buildStatus/icon?job=Pipeline-as-code/DEEP-OC-org/${DEEP_CODE_REPO}/${DEEP_CODE_REPO_BRANCH}"
+DEEP_CODE_REPO_JENKINS_URL="https://jenkins.indigo-datacloud.eu/job/Pipeline-as-code/job/DEEP-OC-org/job/${DEEP_CODE_REPO}/job/${DEEP_CODE_REPO_BRANCH}"
+AI4_DEEPAAS_REPO_URL="https://github.com/ai4os/DEEPaaS"
+AI4_CODE_REPO_JENKINS_BADGE="https://jenkins.services.ai4os.eu/buildStatus/icon?job=AI4OS-hub/${AI4_CODE_REPO}/${AI4_CODE_REPO_BRANCH}"
+AI4_CODE_REPO_JENKINS_URL="https://jenkins.services.ai4os.eu/job/AI4OS-hub/job/${AI4_CODE_REPO}/job/${AI4_CODE_REPO_BRANCH}/"
+
+replace-old-urls metadata.json
+check-old-nc metadata.json
+
+sed -i "s,http://github.com,https://github.com,gI" setup.cfg  # in case "http://" is wrongly given for github.com
+replace-old-urls setup.cfg
+sed -i "s,%2F,/,gI" README.md # replace "%2F" code with "/"
+replace-old-urls README.md
+check-old-nc README.md
+
+git commit -a -m "feat: migration-2, Update files for AI4OS URL values"
+
+# 4. Update Dockerfile, requirements, and test-requirements. MOSTLY MANUAL PROCESS!
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "4. PRETTY MUCH MANUAL PROCESS to Update: "
+echo " * Dockerfile"
+echo " * requirements.txt"
+echo " * test-requirements.txt / requirements-test.txt"
+echo " * commit changes to ${AI4_CODE_REPO_URL}"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+read -p "Press enter to continue"
+
+replace-old-urls Dockerfile
+check-old-nc Dockerfile
+# find the name of "requirements.txt" file for tests
+AI4_CODE_REPO_TEST_REQUIREMENTS="test-requirements.txt"
+ls -1 |grep "requirements-test.txt"
+[[ $? -eq 0 ]] && AI4_CODE_REPO_TEST_REQUIREMENTS="requirements-test.txt"
+# ...........
+# ...........
+# ...........
+
+# 5. Delete/Re-add Jenkinsfile
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "5. Re-create Jenkins CI/CD: "
+echo " * re-create Jenkinsfile"
+echo " * create .sqa/config.yml"
+echo " * create .sqa/docker-compose.yml"
+echo " * create JenkinsConstants.groovy"
+echo " * re-create tox.ini"
+echo " * commit changes to ${AI4_CODE_REPO_URL}"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+read -p "Press enter to continue"
+# create Jenkinsfile from the template
 cp ${SCRIPT_PATH}/cp-Jenkinsfile ./Jenkinsfile
+# check for the base_cpu_tag
+export $(cat ../${DEEP_DOCKERFILE_REPO}/Jenkinsfile |grep -i "base_cpu_tag" |head -n1 |tr -d ' ' |sed 's/"//g')
+# check for the base_gpu_tag
+export $(cat ../${DEEP_DOCKERFILE_REPO}/Jenkinsfile |grep -i "base_gpu_tag" |head -n1 |tr -d ' ' |sed 's/"//g')
+# create JenkinsConstants.groovy from the template
+sed -e "s,DOCKER_BASE_CPU_TAG,${base_cpu_tag},g" \
+    -e "s,DOCKER_BASE_GPU_TAG,${base_gpu_tag},gI" \
+    ${SCRIPT_PATH}/tmpl-JenkinsConstants.groovy > JenkinsConstants.groovy
 mkdir .sqa
+# create .sqa/config.yml from the template
 sed "s,AI4_CODE_REPO,${AI4_CODE_REPO},g" ${SCRIPT_PATH}/tmpl-sqa-config.yml > .sqa/config.yml
 AI4_CICD_DOCKER_IMAGE="indigodatacloud/ci-images:python3.6"
 echo "Please, provide CI/CD Image for the code testing, default is ${AI4_CICD_DOCKER_IMAGE}"
@@ -208,43 +274,14 @@ do
     esac
 done
 echo "[INFO] Configured CI/CD image for the code testing: ${AI4_CICD_DOCKER_IMAGE}"
+# create .sqa/docker-compose.yml from the template
 sed "s,AI4_CICD_DOCKER_IMAGE,${AI4_CICD_DOCKER_IMAGE},g" ${SCRIPT_PATH}/tmpl-sqa-docker-compose.yml > .sqa/docker-compose.yml
-git add Jenkinsfile .sqa/*
-git commit -a -m "feat: migration-2, add Jenkins CI/CD with JePL2"
-git push origin
-
-# Can UPDATE automatically, EXISTING files:
-# metadata.json : replace deepaas_repo, dockerfile_repo, docker_registry_repo, code, jenkins_badge, jenkins_url
-DEEP_DEEPAAS_REPO_URL="https://github.com/indigo-dc/DEEPaaS"
-DEEP_JENKINS_REPO_BADGE="https://jenkins.indigo-datacloud.eu/buildStatus/icon?job=Pipeline-as-code/DEEP-OC-org/${DEEP_DOCKERFILE_REPO}/${DEEP_DOCKERFILE_REPO_BRANCH}"
-DEEP_JENKINS_REPO_URL="https://jenkins.indigo-datacloud.eu/job/Pipeline-as-code/job/DEEP-OC-org/job/${DEEP_DOCKERFILE_REPO}/job/${DEEP_DOCKERFILE_REPO_BRANCH}"
-AI4_DEEPAAS_REPO_URL="https://github.com/ai4os/DEEPaaS"
-AI4_JENKINS_REPO_BADGE="https://jenkins.services.ai4os.eu/buildStatus/icon?job=AI4OS-hub/${AI4_CODE_REPO}/${AI4_CODE_REPO_BRANCH}"
-AI4_JENKINS_REPO_URL="https://jenkins.services.ai4os.eu/job/AI4OS-hub/job/${AI4_CODE_REPO}/job/${AI4_CODE_REPO_BRANCH}/"
-
-replace-old-urls metadata.json
-check-old-nc metadata.json
-
-sed -i "s,http://github.com,https://github.com,gI" setup.cfg  # in case "http://" wrongly given for github.com
-replace-old-urls setup.cfg
-sed -i "s,%2F,/,gI" README.md # replace "%2F" code with "/"
-replace-old-urls README.md
-check-old-nc README.md
-
-exit 1
-
-# Can create with replacement:
-# tox.ini
-# .sqa/config.yml
-# .sqa/docker-compose.yml
-# JenkinsConstants.groovy
-# Delete: .stestr.conf, Jenkinsfile, tox.ini
+# create tox.ini from the template
+sed -e "s,AI4_CODE_REPO_TEST_REQUIREMENTS,${AI4_CODE_REPO_TEST_REQUIREMENTS},gI" \
+    -e "s,AI4_CODE_REPO,${AI4_CODE_REPO},gI" ${SCRIPT_PATH}/tmpl-tox.ini > tox.ini
+# Delete: .stestr.conf as we don't use it anymore
 git rm .stestr.conf
-git rm tox.ini
-
-# Need to UPDATE MANUALLY:
-# Dockerfile
-# requirements.txt
-# test-requirements.txt
-# OR requirements-test.txt
+git add Jenkinsfile JenkinsConstants.groovy tox.ini .sqa/*
+git commit -a -m "feat: migration-4, add Jenkins CI/CD with JePL2 (.sqa, tox.ini)"
+git push origin
 
